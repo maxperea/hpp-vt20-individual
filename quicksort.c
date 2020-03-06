2f5bf86594cb743f72b913a2213e56fb80e6da4d
Max Perea <maxpereaduring@gmail.com>
2020-03-06 16:16:02 +0100

    init

---
+++ quicksort.c
@@ -0,0 +1,18 @@
+/*
+ * File: quicksort.c
+ * ----------------
+ * An optimized implementation of quicksort.
+ *
+ * Individual Assignment
+ * Max Perea Düring
+ * Uppsala University
+ *
+ */
+#include "quicksort.h"
+
+
+void quicksort(void *array, void *cmp_fun){
+
+
+}
+
---
+++ quicksort.h
@@ -0,0 +1,23 @@
+/*
+ * File: quicksort.h
+ * ----------------
+ * An optimized implementation of quicksort.
+ *
+ * Individual Assignment
+ * Max Perea Düring
+ * Uppsala University
+ *
+ */
+#ifndef _quicksort_h
+#define _quicksort_h
+
+/*
+ * Function: quicksort
+ * Usage: quicksort(array, cmp_fun)
+ * -----------------------------------
+ * Sorts the given array.
+ *
+ */
+void quicksort(void *array, void *cmp_fun);
+
+#endif
